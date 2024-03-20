b13d52952f60357a4e794bba58cae1475032f8c6
JRKviteberg <janrogerk@hotmail.com>
2024-03-20 10:13:47 +0100

    CSVReader lagt til

---
+++ PersonDataLibrary/Services/PersonDataService.cs
@@ -0,0 +1,22 @@
+﻿using PersonDataLibrary.Models;
+
+namespace PersonDataLibrary.Services;
+
+public class PersonDataService
+{
+    public IEnumerable<Person> GetPersons()
+    {
+        // opprette list/array semle persondata
+        var persons = new List<Person>();
+        
+        // lese fra csv fil -> List<string[]>
+
+        // tolke innhold -> gjøre string[] -> person
+
+        // opprette person legge til liste
+
+        // return liste persons
+
+        return persons;
+    }
+}
---
+++ PersonDataLibrary/Utilities/CSVReader.cs
@@ -0,0 +1,37 @@
+﻿namespace PersonDataLibrary.Utilities;
+
+public static class CSVReader
+{
+    public static IEnumerable<string[]> ReadFromFile(string fileName)
+    {
+        // oppretter liste med string -> hver linje splittes til string[] og puttes i liste!!!
+        var csvList = new List<string[]>();
+
+        if (!File.Exists(fileName)) 
+            throw new FileNotFoundException($"Finner ikke filen: {fileName}");
+        
+        // Exception !! 
+        try
+        {
+            // using -> (python: with open)
+            using StreamReader reader = new StreamReader(fileName, System.Text.Encoding.UTF8);
+            
+            // header line
+            string? header = reader.ReadLine();
+            while (!reader.EndOfStream)
+            {
+                
+            }
+        }
+        // catch (IOException ioEx) {}
+        catch (Exception e)
+        {
+            // Logge
+            Console.WriteLine(e.ToString());
+            throw;
+        }
+
+        return csvList;
+    }
+    
+}
